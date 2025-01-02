72e5fd24ced1ea8a31d23ce2b720a9e39200a697
Eduardo Ramos <edusilvaramos@hotmail.com>
2025-01-02 09:39:03 +0100

    Eduardo

--- cnxConfig.php
+++ cnxConfig.php
@@ -11,7 +11,7 @@ function returnCnx()
         $connexion = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
         $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // to show the result in the console
-        echo "<script>Returns the number of rows affected by the last SQL statement.log('connexion ok')</script>";
+        echo "<script>console.log('Connexion OK');</script>";
     } catch (PDOException $e) {
         echo "Erreur de connexion : " . $e->getMessage();
     }
